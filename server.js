4f5047948872416a3e1d518b17bcf548dfca5547
Todd Gutschow <Todd Gutschow>
2017-02-21 22:08:53 -0600

    Set port on express app

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
     console.log(req.params);
     res.send('hello world!');
 });
 
-app.listen(8080, function() {
-    console.log('example app listening on port 8080');
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
