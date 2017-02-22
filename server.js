8523803a1931c7602f5c40ff3166188ba5d8e1e2
Todd Gutschow <Todd Gutschow>
2017-02-21 22:12:56 -0600

    Try to access url parameters

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-    console.log(req.params);
+app.get('/', function(req, res) {    
     res.send('hello world!');
 });
+
+app.get('/:time', function(req, res) {
+    console.log(req.params);
+    res.send('hello time!');
+})
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
